dbc56923d0dcbe606218efc30454a530e5f8beaa
Jose Juarez <120photo@gmail.com>
2014-11-09 07:39:28 -0800

    added 'validates :url, uniqueness: true' to website model to do basic validation that the site is unique'

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,8 @@
 class Website < ActiveRecord::Base
   belongs_to :user
   has_many :events
+  validates :url, uniqueness: true
+
   # before_create :does_site_exist
   # TODO seeds fail if does_site_exist is called with before_create when there is no initial site record, must be commented out for seeds to work, fix this...
 
