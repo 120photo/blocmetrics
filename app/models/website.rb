d43c27b1b47c9ffbd026e787a1a742c82391a605
Jose Juarez <120photo@gmail.com>
2014-11-09 19:33:32 -0800

    website verification is now working

--- app/models/website.rb
+++ app/models/website.rb
@@ -3,12 +3,24 @@ class Website < ActiveRecord::Base
   has_many :events
   validates :url, uniqueness: true
 
-  # before_create :does_site_exist
-  # TODO seeds fail if does_site_exist is called with before_create when there is no initial site record, must be commented out for seeds to work, fix this...
-
-  # check if site already exists
-  def does_site_exist
-    Website.where(url: self.url).where(verified: true).exists?
+  def verify!
+    # @website = website.id
+    begin
+      response = HTTParty.get(self.url)
+      doc = Nokogiri::HTML(response)
+      # use snippet below to find key
+      puts doc.xpath("//meta[@name='blocmetrics']").inspect
+      key = doc.xpath("//meta[@name='blocmetrics']").last['value']
+      if key == self.verification_token
+        self.verified = true
+        self.save
+        return true
+      else
+        false
+      end
+    rescue
+      return false
+    end
   end
 
 end
