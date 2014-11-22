59e86e555b699a39cb920f52916facef534f2fb0
Jose Juarez <120photo@gmail.com>
2014-11-22 00:01:28 -0800

    implemented CORS

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::BaseController < ApplicationController
 
+  before_filter :set_headers
+
   respond_to :json
 
   # token based authentication methods
+
+  private
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+    headers['Access-Control-Max-Age'] = '86400'
+  end
 
 end
