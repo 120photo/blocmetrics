d2831fe381927915ebc9bca0b2c69da9a7b2d2be
Jose Juarez <120photo@gmail.com>
2014-11-29 19:18:29 -0800

    made change in verification controller

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -3,14 +3,14 @@ class VerificationsController < ApplicationController
   def update
     @website = Website.find(params[:id])
 
-    AutoVerifySites.perform_async(@website.id)
-
-    redirect_to @website, notice: "We are checking to see if your site is verified."
-    # if @website.verify!
-    #   redirect_to @website, notice: "Successfully verified #{@website.url}"
-    # else
-    #   redirect_to @website, notice: "could not find 'blocmetrics' meta tag in  #{@website.url}"
-    # end
+    # AutoVerifySites.perform_async(@website.id)
+    #
+    # redirect_to @website, notice: "We are checking to see if your site is verified."
+    if @website.verify!
+      redirect_to @website, notice: "Successfully verified #{@website.url}"
+    else
+      redirect_to @website, notice: "could not find 'blocmetrics' meta tag in  #{@website.url}"
+    end
   end
 
 end
