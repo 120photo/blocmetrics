9cca28bce12ab3de9b8b55be37f923030154e5c1
Jose Juarez <120photo@gmail.com>
2014-12-04 01:09:57 -0800

    parse host

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::EventsController < Api::V1::BaseController
       # @event.host = request.env["REMOTE_HOST"]
       #               request.referer
       @event.agent = request.user_agent
-      # @event.uri = URI.parse(request.referer).host
+      @event.uri = URI.parse(request.referer).host
       @event.save
     end
     # EventWorker.perform_async(user_id, event_name, event_Data)
