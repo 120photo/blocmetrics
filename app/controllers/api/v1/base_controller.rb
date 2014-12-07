2b2c50cfc2ec6ab193f6a4d38ac3bea84ef2649b
Jose Juarez <120photo@gmail.com>
2014-12-07 03:03:23 -0800

    no more nagging about cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,26 +4,30 @@ class Api::V1::BaseController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
 
-  before_filter :authenticate_user_from_token!
-  before_filter :authenticate_user!
-
   before_filter :cors_preflight_check
   after_filter :set_headers
 
-  private
+  before_filter :authenticate_user_from_token!
+  before_filter :authenticate_user!
+
 
   def authenticate_user_from_token!
-    token = params[:auth_token].presence
-    user = User.where(authentication_key: token).first
 
-    logger.info user.inspect
+    if request.method != "OPTIONS"
+      token = params[:auth_token]
+      user = User.where(authentication_key: token).first
 
-    if user && Devise.secure_compare(user.authentication_key, token)
-      sign_in user, store: false
-    else
-      render nothing: true, status: :unauthorized
+      # logger.info user.inspect
+
+      if user #&& Devise.secure_compare(user.authentication_key, token)
+        sign_in user, store: false
+      else
+        render json: {error: "Unauthorized"},status: :unauthorized and return
+      end
     end
   end
+
+  private
 
   def set_headers
     headers['Access-Control-Allow-Origin'] = '*'
@@ -33,13 +37,11 @@ class Api::V1::BaseController < ApplicationController
   end
 
   def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Origin, X-Requested-With, X-Prototype-Version, Content-Type'
+    headers['Access-Control-Max-Age'] = '1728000'
+    render text: "" if request.method == "OPTIONS"
   end
 
 end
