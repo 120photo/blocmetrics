b33ef3e8542b02eacfaac97f99e2822061593a34
Jose Juarez <120photo@gmail.com>
2014-12-07 09:33:00 -0800

    moved event creation over to worker

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,28 +4,14 @@ class Api::V1::EventsController < Api::V1::BaseController
   def create
 
     if current_user
-      @event = Event.new
-      @event.name = params[:name]
-      @event.data = params[:data]
-      @event.user = current_user
-      @event.ip = request.env["REMOTE_ADDR"]
-      # @event.host = request.env["REMOTE_HOST"]
-      #               request.referer
-      @event.agent = request.user_agent
-      @event.uri = URI.parse(request.referer).host
-      @event.save
+      EventWorker.perform_async(
+        current_user, params[:name],
+        params[:data],
+        request.env["REMOTE_ADDR"],
+        request.user_agent,
+        URI.parse(request.referer).host
+      )
     end
-    # EventWorker.perform_async(user_id, event_name, event_Data)
-
-    # Create a worker that creates an event for the domain that submitted the request
-
-    # 1. Read about the Rails request object and find a way to extract where the request to this method came from.
-    # The worker should accept the following parameters:
-    # 1. The submitting domain
-    # 2. The event name
-    # 3. current_user's id
-    # 4. any other of custom data you see fit
-    # HTTP_USER_AGENT, REMOTE_ADDR, REQUEST_URI
 
     # Important. if the worker find out that this is an unauthorized request. it won't create the event.
     render nothing: true, status: :created
--- app/workers/event_worker.rb
+++ app/workers/event_worker.rb
@@ -1,8 +1,19 @@
 class EventWorker
   include Sidekiq::Worker
+  require 'uri'
 
-  def perform(event_id)
-    event = Event.new
-    
+  def perform(user, event_name, event_data, event_ip, event_agent, event_uri)
+
+    if Website.where(url: event_uri).where(verified: true)
+      @event = Event.new
+      @event.user = user
+      @event.name = event_name
+      @event.data = event_data
+      @event.ip = event_ip
+      @event.agent = event_agent
+      @event.uri = event_uri
+    else
+      render json: {error: "Site not Verified"},status: :unauthorized and return
+    end
   end
 end
